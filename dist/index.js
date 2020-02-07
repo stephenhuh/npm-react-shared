eb21587cdbeb203602b0feab7723fbb292c6f603
stevie huh <stephenhuh@gmail.com>
2020-02-07 16:34:41 -0500

    init

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 /*
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -13,5 +12,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 */
 
-
-export {App}
+export { App };
